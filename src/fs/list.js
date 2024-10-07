2a8b5d25414bf693e0bc095093ffed0885f49696
anton-glebov <anton.glebov@useinsider.com>
2024-10-07 18:19:24 +0300

    add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import  { readdir } from 'node:fs/promises'
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+    const pathDir = 'src/fs/files';
+    const folder = path.join(path.resolve(), pathDir);
+    try {
+        const files = await readdir(folder);
+        console.log(files);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await list();

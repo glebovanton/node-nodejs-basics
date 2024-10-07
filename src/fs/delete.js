68710e5419ae9a62aea0656285d9bb651ff2584b
anton-glebov <anton.glebov@useinsider.com>
2024-10-07 17:35:28 +0300

    add delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { unlink } from "fs/promises";
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    const currentDir = path.resolve();
+    const pathDir = 'src/fs/files';
+    const file = path.join(currentDir, pathDir, 'fileToRemove.txt');
+
+    try {
+        await unlink(file)
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await remove();

729e022028ad9fda1e8d916f457b326d2e0b496c
anton-glebov <anton.glebov@useinsider.com>
2024-10-07 22:16:01 +0300

    add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { pipeline } from 'stream';
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = 'fileToCalculateHashFor.txt';
+    const pathDir = 'src/hash/files';
+    const file = path.join(path.resolve(), pathDir, fileName);
+    const algorithm = 'sha256'
+    const hash = createHash(algorithm);
+
+    pipeline(
+        createReadStream(file),
+        hash,
+        (err) => {
+            if (err) {
+                console.error('Pipeline failed:', err);
+            } else {
+                const result = hash.digest('hex');
+                console.log(`File: ${file}`);
+                console.log(`${algorithm} hash: ${result}`);
+            }
+        }
+    );
 };
 
 await calculateHash();

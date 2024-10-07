070c8e8f4250bd1a966b7cbc501a9e5205895091
anton-glebov <anton.glebov@useinsider.com>
2024-10-07 23:08:22 +0300

    add write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import { createWriteStream } from 'node:fs'
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+    const fileName = 'fileToWrite.txt';
+    const pathDir = 'src/streams/files';
+    const file = path.join(path.resolve(), pathDir, fileName);
+    const writeStream = createWriteStream(file);
+
+    process.stdin.setEncoding('utf8');
+
+    process.stdout.write(`Type you want to save in file ${file}:\n`);
+
+    process.stdin.on('data', (input) => {
+        const trimmedInput = input.trim();
+
+        if (trimmedInput === 'exit') {
+            process.exit();
+        }
+
+        writeStream.write(trimmedInput + '\n');
+    });
 };
 
 await write();

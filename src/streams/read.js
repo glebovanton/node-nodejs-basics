a8264d3e5c0101b7c440bbed23f2a1787af76a8c
anton-glebov <anton.glebov@useinsider.com>
2024-10-07 22:47:48 +0300

    add read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from "fs";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+    const fileName = 'fileToRead.txt';
+    const pathDir = 'src/streams/files';
+    const file = path.join(path.resolve(), pathDir, fileName);
+    const readable = createReadStream(file);
+
+    readable.on('readable', () => {
+        let chunk
+        while ((chunk = readable.read()) !== null ) {
+            process.stdout.write(chunk);
+        }
+    })
+
+    readable.on('error', (err) => {
+        console.error('Stream failed:', err);
+    });
 };
 
 await read();

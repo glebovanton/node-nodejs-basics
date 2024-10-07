871c50acc74fc661c4b7c9130556d667e141105f
anton-glebov <anton.glebov@useinsider.com>
2024-10-07 23:57:13 +0300

    add decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import zlib from 'node:zlib';
+import path from "path";
+
 const decompress = async () => {
-    // Write your code here 
+    const pathDir = 'src/zip/files';
+    const zipFile = path.join(path.resolve(), pathDir, 'archive.gz');
+    const zipFileStream = createReadStream(zipFile);
+    const file = path.join(path.resolve(), pathDir, 'fileToCompress.txt');
+    const fileStream = createWriteStream(file);
+    const gunzip = zlib.createGunzip();
+
+    zipFileStream
+        .pipe(gunzip)
+        .pipe(fileStream)
+        .on('error', (err) => {
+            console.error('Error decompressing:', err);
+        });
 };
 
 await decompress();
